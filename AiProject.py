6a4a64c201aeedc0a2e9ba91c0b60d449f6d97f5
Yousef <yousefshafik234@yahoo.com>
2021-05-19 02:46:10 +0200

    added the ability to creat edges and reset the canvas

--- AiProject.py
+++ AiProject.py
@@ -17,9 +17,36 @@ class MyBoxLayout(Widget):
     alphabetOrder = 'A'
     LabelDict = {}
     circleDict = {}
+    graph = {}
+    firstNode = None
+    count = 1
+
     def spinner_clicked(self,value):
         pass
 
+    def drawNode(self,x,y):
+        self.obj = InstructionGroup()
+        self.obj.add(Color(rgb=(1,0,0)))
+        self.obj.add(Ellipse(pos=(x-RADIUS,y-RADIUS),size=(RADIUS*2,RADIUS*2)))
+        self.ids.canvasID.canvas.add(self.obj)
+        l = Label(text= self.alphabetOrder, pos = [x-RADIUS,y-RADIUS],font_size = 15,color = (0,0,0,1),
+        size = (RADIUS*2,RADIUS*2),pos_hint = (1,1),size_hint=(0.2,0.2))
+        self.LabelDict[self.alphabetOrder] = l
+        self.circleDict[self.alphabetOrder] = self.obj
+        self.add_widget(l)
+        self.graph[self.alphabetOrder] = {}
+        self.alphabetOrder = chr(ord(self.alphabetOrder) + 1)
+
+    def clearNode(self,x,y):
+        for key in self.LabelDict:
+                    if x > self.LabelDict[key].x and y > self.LabelDict[key].y and x < self.LabelDict[key].x+RADIUS*2 and y<self.LabelDict[key].y+RADIUS*2:
+                        l = self.LabelDict.pop(key)
+                        item = self.circleDict.pop(key)
+                        self.remove_widget(l)
+                        self.ids.canvasID.canvas.remove(item)
+                        break
+    
+    
     def clearStuff(self):
         for key in self.LabelDict:
             self.remove_widget(self.LabelDict[key])
@@ -27,6 +54,8 @@ class MyBoxLayout(Widget):
         self.LabelDict = {}
         self.circleDict = {}
         self.alphabetOrder = 'A'
+        self.graph = {}
+        
         with self.ids.canvasID.canvas:
             Color(1,1,1,1, mode = 'rgba')
             Rectangle(size = self.ids.canvasID.size,pos = self.ids.canvasID.pos)
@@ -38,34 +67,48 @@ class MyBoxLayout(Widget):
 
             if(self.ids.node_button.state == 'down'):
 
-                self.obj = InstructionGroup()
-                self.obj.add(Color(rgb=(1,0,0)))
-                self.obj.add(Ellipse(pos=(touch.x-RADIUS,touch.y-RADIUS),size=(RADIUS*2,RADIUS*2)))
-                self.ids.canvasID.canvas.add(self.obj)
-                l = Label(text= self.alphabetOrder, pos = [touch.x-RADIUS,touch.y-RADIUS],font_size = 15,color = (0,0,0,1),
-                size = (RADIUS*2,RADIUS*2),pos_hint = (1,1),size_hint=(0.2,0.2))
-                self.LabelDict[self.alphabetOrder] = l
-                self.circleDict[self.alphabetOrder] = self.obj
-                self.alphabetOrder = chr(ord(self.alphabetOrder) + 1)
-                self.add_widget(l)
+                self.drawNode(touch.x,touch.y)
                 #create a new Node
                 return
 
             elif(self.ids.rightArrowID.state == 'down'):
-                with self.ids.canvasID.canvas:
-                    self.canvas.add(Color(rgb=(0,0,0)))
-                    myLine = Line()
-                    print(self.nodes)
-                    return
+                for key in self.LabelDict:
+                        if touch.x > self.LabelDict[key].x and touch.y > self.LabelDict[key].pos[1] and touch.x < self.LabelDict[key].pos[0]+RADIUS*2 and touch.y<self.LabelDict[key].pos[1]+RADIUS*2:
+                            if self.count == 2:  
+                                self.count  = 1
+                                self.obj = InstructionGroup()
+                                self.obj.add(Color(0,0,0,1,mode='rgba'))
+                                self.obj.add(Line(points=[self.LabelDict[self.firstNode].x+RADIUS, self.LabelDict[self.firstNode].y+RADIUS,
+                                self.LabelDict[key].x +RADIUS, self.LabelDict[key].y+RADIUS],width = 3))
+                                self.graph[self.firstNode][key] = self.obj
+                                self.ids.canvasID.canvas.add(self.obj)
+
+                                firstLabel = self.LabelDict[key]
+                                secondLabel = self.LabelDict[self.firstNode]
+
+                                item1 = self.circleDict[key]
+                                item2  = self.circleDict[self.firstNode]
+                                self.remove_widget(firstLabel)
+                                self.remove_widget(secondLabel)
+                                self.ids.canvasID.canvas.remove(item1)
+                                self.ids.canvasID.canvas.remove(item2)
+
+                                self.ids.canvasID.canvas.add(item1)
+                                self.ids.canvasID.canvas.add(item2)
+                                self.add_widget(firstLabel)
+                                self.add_widget(secondLabel)
+                                
+    
+                                return 
+                            else:
+
+                                self.count += 1
+                                self.firstNode = key
+                                return 
+                return 
 
             elif(self.ids.clearButtonID.state == 'down'):
-                for key in self.LabelDict:
-                    if touch.x > self.LabelDict[key].x and touch.y > self.LabelDict[key].pos[1] and touch.x < self.LabelDict[key].pos[0]+60 and touch.y<self.LabelDict[key].pos[1]+60:
-                        l = self.LabelDict.pop(key)
-                        item = self.circleDict.pop(key)
-                        self.remove_widget(l)
-                        self.ids.canvasID.canvas.remove(item)
-                        break
+                self.clearNode(touch.x,touch.y)
                 return
                 #delete Node
                 
